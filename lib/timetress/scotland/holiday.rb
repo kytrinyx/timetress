e0705204796e784b95d3d486e0fda4f96cb59ea5
John Mertens <john.mertens@gmail.com>
2014-08-24 09:01:18 +0100

    Add the royal proclamation functionality

--- lib/timetress/scotland/holiday.rb
+++ lib/timetress/scotland/holiday.rb
@@ -12,9 +12,14 @@ module Timetress
       end
 
       def spring_holiday(year)
-        last_monday_in(MAY, year)
+        if year == 2012
+          # Special case for the Queen's Diamond Jubilee
+          Date.new(2012, 6, 4)
+        else
+          last_monday_in(MAY, year)
+        end
       end
-      alias_method :spring_holiday, :victoria_day
+      alias_method :victoria_day, :spring_holiday
 
       def summer_holiday(year)
         first_monday_in(AUGUST, year)
@@ -32,7 +37,7 @@ module Timetress
         # 4 days to the 1st Sunday + 3 more weeks = 25 days
         ash_wednesday(year) + 25
       end
-      alias_method :mothering_day :mothersday
+      alias_method :mothering_day, :mothersday
 
       def fathersday(year)
         third_sunday_in(JUNE, year)
@@ -49,10 +54,47 @@ module Timetress
           st_andrews_day(year),
           christmas(year),
           boxing_day(year)
+        ].uniq.sort
+      end
+
+      def bank_holidays(year)
+        official_holidays(year).map{ |d| royal_proclamation(d) }
+      end
+
+      def royal_proclamation(holiday)
+        # Scotland has two two-day holidays. We use these below.
+        special_2nd_day_holidays = [
+          boxing_day(holiday.year),
+          new_year_holiday(holiday.year)
         ]
+
+
+        if holiday.sunday?
+          # If the second day of a two day holiday falls on a Sunday,
+          # then the Monday & Tuesday become bank holidays.
+          if special_2nd_day_holidays.include? holiday
+            holiday + 2
+          else
+            holiday + 1
+          end
+        elsif holiday.saturday?
+          holiday + 2
+        else
+          # If the second day of a two day holiday is on a Monday
+          # then it gets the Monday and the second day gets the
+          # Tuesday.
+          if holiday.monday? &&
+            special_2nd_day_holidays.include?(holiday)
+
+            holiday + 1
+          else
+            holiday
+          end
+        end
       end
 
       private
+
       def first_monday_in(month, year)
         first_week = Date.new(year, month, 1)
         first_week + days_until_monday(first_week)
